bb2c711d0b216e69568a10477a769f935b989d08
--global <simone.mosconi2@studio.unibo.it>
2025-02-05 23:27:23 +0100

    update notification-list page

--- source/sneakersShop/sito/php/user/template/notification-list.php
+++ source/sneakersShop/sito/php/user/template/notification-list.php
@@ -1,12 +1,11 @@
 <h1 class="fw-semibold mt-5">NOTIFICHE</h1>
-<ol class="list-group list-unstyled mt-3">
+<ol class="list-group list-unstyled mt-3" id="notification-list">
     <?php foreach($templateParams["notifications"] as $notif): ?>
-        <li class="list-group-item">
+        <li class="list-group-item <?php if($notif["stato"] == 'da_leggere') echo 'bg-body-secondary' ?>">
             <div class="text-break">
                 <div class="d-flex w-100">
                     <h2 class="mb-3 flex-grow-1 h5"><?php echo $notif["titolo"]; ?></h2>
-                    <!-- TODO: valuta se usare AJAX per cambiare lo stato della mail -->
-                    <a class="btn btn-sm bi bi-envelope-open py-0 fs-3 icon" aria-label="Segna Come letto o non letto"></a>
+                    <a class="btn btn-sm bi <?php echo $notif["stato"] == 'da_leggere' ? 'bi-envelope-open' : 'bi-envelope' ; ?> py-0 fs-3 icon <?php echo $notif["stato"]; ?>" id="<?php echo $notif["idnotifica"]; ?>" aria-label="Segna Come già letto/da leggere"></a>
                     <small class="text-body-secondary mt-2"><?php echo $notif["data"]; ?></small>
                 </div>
                 <p class="mb-1"><?php echo $notif["messaggio"]; ?></p>
