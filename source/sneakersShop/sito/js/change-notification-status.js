7d6fa332eeab4b3d26ca8aef659d0ffd956a53db
--global <simone.mosconi2@studio.unibo.it>
2025-02-07 02:51:54 +0100

    change variable name

--- source/sneakersShop/sito/js/change-notification-status.js
+++ source/sneakersShop/sito/js/change-notification-status.js
@@ -29,12 +29,12 @@ async function toggleStatus(node, currStatus, newStatus, currIcon, newIcon) {
     }
 }
 
-document.querySelectorAll('a.letta, a.da_leggere').forEach(link => {
-    link.addEventListener('click', async (e) => {
-        if(link.classList.contains('da_leggere')) {
-            toggleStatus(link, 'da_leggere', 'letta', 'bi-envelope-open', 'bi-envelope');
+document.querySelectorAll('a.letta, a.da_leggere').forEach(btn => {
+    btn.addEventListener('click', async (e) => {
+        if(btn.classList.contains('da_leggere')) {
+            toggleStatus(btn, 'da_leggere', 'letta', 'bi-envelope-open', 'bi-envelope');
         } else {
-            toggleStatus(link, 'letta', 'da_leggere', 'bi-envelope', 'bi-envelope-open');
+            toggleStatus(btn, 'letta', 'da_leggere', 'bi-envelope', 'bi-envelope-open');
         }
         e.preventDefault();
     });
