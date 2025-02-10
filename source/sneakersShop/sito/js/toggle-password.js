f6b6c3b524b08898f35707476e5c443fb00975be
--global <simone.mosconi2@studio.unibo.it>
2025-02-10 03:21:17 +0100

    add script to toggle password visibility

---
+++ source/sneakersShop/sito/js/toggle-password.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// toggles the password visibility after the eye icon is clicked
+document.querySelectorAll('button.toggle-password').forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        const btn = e.currentTarget;
+        const pwd = btn.parentElement.querySelector('input.pwd-input');
+        const new_type = pwd.getAttribute('type') === 'password' ? 'text' : 'password';
+        pwd.setAttribute('type', new_type);
+
+        btn.querySelector('i').classList.toggle('bi-eye');
+        btn.querySelector('i').classList.toggle('bi-eye-slash');
+    });
+});
+
+// sets the password input's type attribute back to "password" before submitting
+document.querySelector('form.form-with-pwd').addEventListener('submit', (e) => {
+    e.currentTarget.querySelectorAll('input.pwd-input').forEach((input) => {
+        input.setAttribute('type', 'password');
+    });
+});
