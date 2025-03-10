c3ecfc2541df1f0b6b55f66088bd81279571d2e7
Nicolo Morini <nicolo.morini2@studio.unibo.it>
2025-03-10 11:23:59 +0100

    modified products.css to handle long text in products cards

--- source/sneakersShop/sito/php/config/config.php
+++ source/sneakersShop/sito/php/config/config.php
@@ -2,10 +2,10 @@
 
 // Database credentials
 define("HOST", "localhost");
-define("USER", "user");
+define("USER", "root");
 define("PASSWORD", "");
 define("DATABASE", "ns_kicks");
-define("PORT", 3306);
+define("PORT", 3307);
 
 define("IMG_DIR", "img/");
 define("BASE_PATH", 'http://' . $_SERVER['HTTP_HOST'] . '/Progetto_Web/source/sneakersShop/sito/');
