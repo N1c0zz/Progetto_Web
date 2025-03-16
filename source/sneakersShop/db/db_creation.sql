f7ed2693496d762fd79de1a18cd787a3c3c5476e
--global <simone.mosconi2@studio.unibo.it>
2025-03-16 18:30:48 +0100

    fix conflict in the user creation query

--- source/sneakersShop/db/db_creation.sql
+++ source/sneakersShop/db/db_creation.sql
@@ -5,7 +5,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 CREATE SCHEMA IF NOT EXISTS `ns_kicks` DEFAULT CHARACTER SET utf8;
 USE `ns_kicks`;
 
-CREATE USER 'user'@'localhost' IDENTIFIED BY '';
+CREATE USER 'ns_user'@'localhost' IDENTIFIED BY '';
 
 -- -----------------------------------------------------
 -- Table `utenti`
