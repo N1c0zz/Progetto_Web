2fffae38bf1c2226cce212af8a820789856b4a1e
Simone Mosconi <simone.mosconi2@studio.unibo.it>
2025-06-02 22:29:46 +0200

    .

--- source/sneakersShop/sito/php/get-new-notifications.php
+++ source/sneakersShop/sito/php/get-new-notifications.php
@@ -12,8 +12,8 @@ if(isset($_POST["currentAmount"])) {
     if ($missingNotifs > 0) {
         $result["new-notifications"] = true;
         $result["notifications"] = array_slice($notifs, 0, $missingNotifs);
-        header('Content-Type application/json');
     }
+    header('Content-Type: application/json');
     echo json_encode($result);
 }
 
